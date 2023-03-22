6caf2109ad1d0377e4641ef41dbc7a1e357045f8
Michal Kapinus <ikapinus@fit.vutbr.cz>
2023-03-22 16:32:03 +0100

    feat: added template for the network application

---
+++ src/python/era_5g_network_application_template/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='era_5g_network_application_template',
+      version='0.0.1',
+      description='Standalone variant of object detection NetApp',
+      author='Amy Pond',
+      author_email='amy.pond@tardis.earth',
+      packages=['era_5g_network_application_template'],
+#      entry_points = {
+#              'console_scripts': [
+#                  'era_5g_network_application_template = era_5g_network_application_template.interface:main',                  
+#              ],              
+#          },
+     )
